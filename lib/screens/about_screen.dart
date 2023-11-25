91a4e9ea768b18415c1a9b4ac9b4413371911745
aidinidk <aidin200382@gmail.com>
2023-11-25 13:01:45 +0330

    added link to other apps and socials

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    void launchOwner() async {
+      if (!await launchUrl(Uri.parse("https://www.instagram.com/kaz_iran/"),
+          mode: LaunchMode.externalApplication)) {
+        throw 'Could not launch ';
+      }
+    }
+
+    void launchInstgram() async {
+      if (!await launchUrl(Uri.parse("https://www.instagram.com/idinkz/"),
+          mode: LaunchMode.externalApplication)) {
+        throw 'Could not launch ';
+      }
+    }
+
+    void launchTelegram() async {
+      if (!await launchUrl(Uri.parse("https://www.t.me/idinkz/"),
+          mode: LaunchMode.externalApplication)) {
+        throw 'Could not launch ';
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('About'),
@@ -22,18 +44,36 @@ class AboutScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Text(
-                  'Find me Here :',
-                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                        color: Theme.of(context).colorScheme.onBackground,
+                const Text('This app was made for :'),
+                InkWell(
+                  onTap: launchOwner,
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        'assets/images/instgram.png',
+                        width: 50,
                       ),
+                      const SizedBox(width: 10),
+                      const Text('Go to'),
+                    ],
+                  ),
                 ),
                 Expanded(
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.end,
                     children: [
+                      Text(
+                        'My socials : ',
+                        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                              color: Theme.of(context).colorScheme.onBackground,
+                            ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
                       InkWell(
-                        onTap: () {},
+                        onTap: launchInstgram,
                         child: Row(
                           children: [
                             Image.asset(
@@ -41,11 +81,12 @@ class AboutScreen extends StatelessWidget {
                               width: 50,
                             ),
                             const SizedBox(width: 10),
-                            const Text('@idinkz'),
+                            const Text('Go to'),
                           ],
                         ),
                       ),
                       InkWell(
+                        onTap: launchTelegram,
                         child: Row(
                           children: [
                             Image.asset(
@@ -53,7 +94,7 @@ class AboutScreen extends StatelessWidget {
                               width: 50,
                             ),
                             const SizedBox(width: 10),
-                            const Text('@idinkz'),
+                            const Text('Go to'),
                           ],
                         ),
                       ),
